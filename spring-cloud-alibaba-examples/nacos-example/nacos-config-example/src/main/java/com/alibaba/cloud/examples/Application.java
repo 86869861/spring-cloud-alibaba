e83b3307c630a1b68e23df3525bc4dc7a762fa89
IT_KF_001 <IT_KF_001@yuri>
2020-08-28 19:46:21 +0800

    nacos 服务注册发现与配置中心例子

--- spring-cloud-alibaba-examples/nacos-example/nacos-config-example/src/main/java/com/alibaba/cloud/examples/Application.java
+++ spring-cloud-alibaba-examples/nacos-example/nacos-config-example/src/main/java/com/alibaba/cloud/examples/Application.java
@@ -32,6 +32,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -42,6 +43,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author xiaojing, Jianwei Mao
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
